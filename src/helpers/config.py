d69967ad759608e9dfaf92c32fa7f50087d5f370
MohamedIbrahim-20 <70908670+MohamedIbrahim-20@users.noreply.github.com>
2025-10-09 02:59:09 +0300

    Update configuration settings in config.py to include default values and new parameters
    - Set default values for APP_NAME and APP_VERSION.
    - Added new configuration options for file handling, including allowed extensions, maximum file size, and default chunk size.
    - Introduced RAG-related settings for model configuration, including default LLM and embedding models, chunk size, overlap, and top K settings.

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -3,12 +3,20 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     # model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     
-    APP_NAME: str
-    APP_VERSION: str
+    APP_NAME: str = "Ebla-RAG"
+    APP_VERSION: str = "1.0.0"
     
-    FILE_ALLOWED_EXTENSIONS: list
-    MAX_FILE_SIZE: int  # in MB
-    FILE_DEFAULT_CHUNK_SIZE: int  # in bytes
+    FILE_ALLOWED_EXTENSIONS: list = ["application/pdf", "text/plain"]
+    MAX_FILE_SIZE: int = 10  # in MB
+    FILE_DEFAULT_CHUNK_SIZE: int = 8192  # in bytes
+    
+    # RAG Configuration
+    DEFAULT_LLM_MODEL: str = "microsoft/DialoGPT-medium"
+    DEFAULT_EMBEDDING_MODEL: str = "all-MiniLM-L6-v2"
+    DEFAULT_CHUNK_SIZE: int = 100
+    DEFAULT_CHUNK_OVERLAP: int = 20
+    DEFAULT_TOP_K: int = 3
+    
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
